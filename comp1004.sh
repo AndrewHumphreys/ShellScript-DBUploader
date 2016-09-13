f77d5db86cd4e0dd67f9215cca9638989a2b841a
AndrewHumphreys <Andrew.Humphreys@student.curtin.edu.au>
2016-09-13 22:05:12 +0800

    Update comp1004.sh
    Updated location of dropbox_uploader.sh

--- comp1004.sh
+++ comp1004.sh
@@ -6,13 +6,13 @@ if [ $CHOICE = upload ]
 
 	then
 	# Upload Command
-	~/dropbox_uploader.sh -p -h upload ~/COMP1004 /
+	~/Dropbox-Uploader/dropbox_uploader.sh -p -h upload ~/COMP1004 /
 	
 	elif [ $CHOICE = download ]
 	then
 	# Download Command
 	rm -r ~/COMP1004
-	~/dropbox_uploader.sh -p -h download /COMP1004 ~/
+	~/Dropbox-Uploader/dropbox_uploader.sh -p -h download /COMP1004 ~/
 	
 	else
 	printf "(Incorrect option)"
